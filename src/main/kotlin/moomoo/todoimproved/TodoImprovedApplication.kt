a83a7848429cab54ca352b664a7981587568059e
moomoo <anwns1211@gmail.com>
2024-06-25 16:22:35 +0900

    :tada: 프로젝트 시작

---
+++ src/main/kotlin/moomoo/todoimproved/TodoImprovedApplication.kt
@@ -0,0 +1,11 @@
+package moomoo.todoimproved
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoImprovedApplication
+
+fun main(args: Array<String>) {
+	runApplication<TodoImprovedApplication>(*args)
+}
---
+++ src/test/kotlin/moomoo/todoimproved/TodoImprovedApplicationTests.kt
@@ -0,0 +1,13 @@
+package moomoo.todoimproved
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoImprovedApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
